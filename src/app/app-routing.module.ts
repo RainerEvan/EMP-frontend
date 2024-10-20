ac7eba1615a44ae59919a856ebbace705ec482a0
RainerEvan <rainerevan@gmail.com>
2024-10-21 00:37:44 +0700

    Initialize Starting Point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './view/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: '', 
+    component: MainComponent,
+    children: [
+      { 
+        path: 'home', 
+        loadChildren: () => import('./view/home/home.module').then(m => m.HomeModule) 
+      }, 
+      { 
+        path: 'employee', 
+        loadChildren: () => import('./view/employee/employee.module').then(m => m.EmployeeModule) 
+      },
+    ]
+  },
+  { 
+    path: 'login', 
+    loadChildren: () => import('./view/login/login.module').then(m => m.LoginModule) 
+  },
+  { 
+    path: '**', 
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { MainComponent } from './view/main/main.component';
+import { HomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
